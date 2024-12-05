7bcd4a978fca6e27d5086fae30acd2b0cf5aacd2
willschreyer <wschreye@villanova.edu>
2024-12-05 11:01:26 -0500

    api.js

---
+++ Project3/src/api.js
@@ -0,0 +1,11 @@
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+};
+
+const getApi = () => {
+    fetch("https://api.currencyfreaks.com/v2.0/rates/latest?base=gbp&symbols=cny,gbp,inr,jpy,usd,cad,eur&apikey=09de422e23c341969b442cb6348a9e46", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+  };
