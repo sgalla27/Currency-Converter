54b53bbce6e0ed32d0cfbc50c2304583cc72f243
willschreyer <wschreye@villanova.edu>
2024-12-09 09:03:01 -0500

    api.js

--- Project3/src/api.js
+++ Project3/src/api.js
@@ -1,11 +1,40 @@
-var requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
-};
+class Freecurrencyapi {
+  baseUrl = 'https://api.freecurrencyapi.com/v1/';
 
-const getApi = () => {
-    fetch("https://api.currencyfreaks.com/v2.0/rates/latest?base=gbp&symbols=cny,gbp,inr,jpy,usd,cad,eur&apikey=09de422e23c341969b442cb6348a9e46", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
-  };
+  constructor(apiKey = '') {
+      this.headers = {
+          apikey: fca_live_jL5hrilxX1Sd47JRrMUCE2mI5b1kHxJtbdP6e5Vv
+      };
+  }
+
+  call (endpoint, params = {}) {
+      const paramString = new URLSearchParams({
+          ...params
+      }).toString();
+
+      return fetch(`${this.baseUrl}${endpoint}?${paramString}`, { headers: this.headers })
+          .then(response => response.json())
+          .then(data => {
+              return data;
+          });
+  }
+
+  status () {
+      return this.call('status');
+  }
+
+  currencies (params) {
+      return this.call('currencies', params);
+  }
+
+  latest (params) {
+      return this.call('latest', params);
+  }
+
+  historical (params) {
+      return this.call('historical', params);
+  }
+
+}
+
+export default Freecurrencyapi;
